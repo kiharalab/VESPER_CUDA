809316d3e5dae71a594aacfd294828845d1bae57
Han Zhu <zhuhan568@outlook.com>
2024-07-05 10:05:53 -0400

    Cap the ATOM number at 99999 for output pdb files

--- utils/fileio.py
+++ utils/fileio.py
@@ -3,8 +3,7 @@ from Bio.PDB import PDBParser, MMCIFParser
 from Bio.PDB.PDBIO import PDBIO
 from TEMPy.maps.map_parser import MapParser
 import TEMPy.math.vector as Vector
-
-import os
+from utils.utils import replace_atom_numbers_with_regex
 
 
 def save_rotated_pdb(input_pdb, rot_mtx, real_trans, save_path, model_num):
@@ -34,9 +33,11 @@ def save_rotated_pdb(input_pdb, rot_mtx, real_trans, save_path, model_num):
             f.write(line)
         f.write("ENDMDL\n")
 
+    # process large pdb files
+    replace_atom_numbers_with_regex(save_path)
+
 
 def save_rotated_mrc(mrc_path, rot_vec, real_trans, save_path):
-
     emap = MapParser.readMRC(mrc_path)
     center = Vector.Vector(0, 0, 0)
 
@@ -60,15 +61,15 @@ def save_rotated_mrc(mrc_path, rot_vec, real_trans, save_path):
 
 
 def save_vec_as_pdb(
-    origin,
-    sampled_mrc_vec,
-    sampled_mrc_data,
-    score_arr,
-    score,
-    sample_width,
-    trans,
-    save_path,
-    rank,
+        origin,
+        sampled_mrc_vec,
+        sampled_mrc_data,
+        score_arr,
+        score,
+        sample_width,
+        trans,
+        save_path,
+        rank,
 ):
     dim = sampled_mrc_data.shape[0]
 
@@ -92,8 +93,8 @@ def save_vec_as_pdb(
             natm += 1
 
             real_coord = (
-                position + sampled_mrc_vec[position[0]][position[1]][position[2]]
-            ) * sample_width + add  # center of voxel plus unit vector
+                                 position + sampled_mrc_vec[position[0]][position[1]][position[2]]
+                         ) * sample_width + add  # center of voxel plus unit vector
             atom_header = f"ATOM{natm:>7d}  CB  ALA{nres:>6d}    "
             atom_content = f"{real_coord[0]:8.3f}{real_coord[1]:8.3f}{real_coord[2]:8.3f}{1.0:6.2f}{score_arr[position[0], position[1], position[2]]:6.2f}"
             pdb_file.write(atom_header + atom_content + "\n")
@@ -102,14 +103,14 @@ def save_vec_as_pdb(
 
 
 def save_map_as_pdb(origin, data, sample_width, trans, save_path):
-    dim = data.shape[0] # dimension of map
+    dim = data.shape[0]  # dimension of map
     origin = np.array([origin[0], origin[1], origin[2]])
     trans = np.array(trans)
     trans = np.where(trans > 0.5 * dim, trans - dim, trans)
     offset = origin - trans * sample_width
 
-    natm = 1    # atom number
-    nres = 1    # residue number
+    natm = 1  # atom number
+    nres = 1  # residue number
 
     with open(save_path, "w") as pdb_file:
         non_zero_dens_index = np.transpose(np.nonzero(data))
